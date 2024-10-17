fd5eb4f20332d2e09e56b968b04383c7acbfeea0
KajanthanSK <kajanthan@spil.com.au>
2024-10-17 23:18:41 +0530

    triangle created

--- 2D_Space/OpenGL/OpenGL/main.cpp
+++ 2D_Space/OpenGL/OpenGL/main.cpp
@@ -7,6 +7,14 @@
 #define SCREENWIDTH 1000
 #define SCREENHEIGHT 800
 
+float vertices[] =
+{
+	/* Position */		
+	-0.5f,-0.5f,0.0f,			
+	 0.5f,-0.5f,0.0f,			
+	 0.0f, 0.5f,0.0f,	
+};
+
 int main()
 {
 	/* Initialize GLFW */
@@ -33,10 +41,29 @@ int main()
 		glfwTerminate();
 	}
 
+	/* Buffers */
+	unsigned int VBO, VAO;
+
+	glGenVertexArrays(1, &VAO);
+
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+	/* Position Attribute */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 
